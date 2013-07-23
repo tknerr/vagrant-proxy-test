3929710d0923ae82a27fa368d0a99b2a462831c9
Torben <tkn@zuehlke.com>
2013-07-23 23:29:52 +0200

    add NO_PROXY, configure apt proxy, check if proxy url is passed to script

--- set_proxy.sh
+++ set_proxy.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 PROXY=$1
+if [ -z "$PROXY" ]; then
+  echo "Need to pass the Proxy URL as a parameter"
+  exit 1
+fi
 
 # set proxy in /etc/profile.d
 echo "setting proxy to inside the Vagrant VM to '$PROXY'"
 cat <<EOF | sudo tee /etc/profile.d/proxy.sh
 export HTTP_PROXY=$PROXY
 export HTTPS_PROXY=$PROXY
+export NO_PROXY=localhost,127.0.0.1
 EOF
 
-# source proxy settings
+# set apt proxy
+cat <<EOF | sudo tee /etc/apt/apt.conf
+Acquire { Retries "0"; HTTP { Proxy "$PROXY"; }; };
+Acquire { Retries "0"; HTTPS { Proxy "$PROXY"; }; };
+EOF
+
+# source proxy settings and print them
 source /etc/profile.d/proxy.sh
 echo "HTTP_PROXY is $HTTP_PROXY"
 echo "HTTPS_PROXY is $HTTPS_PROXY"
+echo "NO_PROXY is $NO_PROXY"
 
-# TODO: set apt / yum proxy
-echo "TODO: set apt/yum proxy"
-
-# just for testing 
-echo 'echo "foo says HTTPS_PROXY is $HTTPS_PROXY and $0 was called with param $1"' | sudo tee /tmp/foo.sh
-sudo chmod +x /tmp/foo.sh
-echo 'calling foo.sh directly'
-/tmp/foo.sh 'directly'
-echo 'calling foo.sh via bash'
-cat /tmp/foo.sh | sudo -E bash -s -- 'via_bash'
 
